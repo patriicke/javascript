b63e792a7bd7208727faf5a4829ed621b33638ab
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-04 23:00:36 +0200

    This is all about the local storage

---
+++ LOCAL STORAGE, COOKIES AND SESSIONS/local storage/script.js
@@ -0,0 +1,3 @@
+localStorage.setItem("name", "Patrick NDAYAMBAJE"); //Setting a local storage value
+localStorage.removeItem("name"); //Removing an item from the localstorage
+// console.log(localStorage.getItem("name")); // This is using the getItem() to get the value assigned to the key passed through this function in the localstorage
